6e8b995048257e1dbe3a709aa9d649f8e1907a9f
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-06-15 23:14:52 -0400

    started firmware

--- firmware/index/index.ino
+++ firmware/index/index.ino
@@ -1,4 +1,10 @@
+String[] prescripts = ["Slam down with weight, topple the body", "Lay vertical the end, insert up the wick", "Lay the blade on its side, slice like a severed breath", "Swing to fell, have it meet the ground", "Aim toward a point, let it echo within", "Carve at a low slant, peel what remains", "Destroy the sound, crush flat the thought", "Stab the silence's heart, penetrate the memory", "With tempered secret, cut the form", ];
 
+int prescriptsLen = 8;
+
+String currentprescript = "";
+
+bool isExecuting = false;
 
 Adafruit_SSD1305 display(128, 64)
 
@@ -10,4 +16,38 @@ Adafruit_SSD1305 display(128, 64)
 
 #define BUTTON 2
 #define BUZZER 8
+
+
+
+void setup(){
+  
+}
+
+void loop(){
+
+}
+
+
+//function to define what happens when a prescript is delivered
+void deliver(){
+
+  if (isExecuting){
+    return;
+  }
+
+  isExecuting = true;
+
+  int randNum = random (0, prescriptsLen);
+
+  currentPrescript = prescripts[randNum];
+
+  
+}
+
+//function for action on clear
+void clear(){
+  isExecuting = false;
+
+  currentPrescipt = 
+}
 
