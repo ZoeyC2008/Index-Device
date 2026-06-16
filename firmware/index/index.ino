1c4195b5a6825ee4277ac675ad54ae64897bd674
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-06-16 11:18:18 -0400

    arduino firmware

--- firmware/index/index.ino
+++ firmware/index/index.ino
@@ -6,6 +6,8 @@ String currentprescript = "";
 
 bool isExecuting = false;
 
+bool isReceived = true;
+
 Adafruit_SSD1305 display(128, 64)
 
 #define OLED_CLK 4
@@ -15,16 +17,29 @@ Adafruit_SSD1305 display(128, 64)
 #define OLED_DC 3
 
 #define BUTTON 2
+
 #define BUZZER 8
 
 
 
 void setup(){
-  
+  pinMode(OLEF_CLK, OUTPUT);
+  pinMode(OLED_MOST, OUTPUT);
+  pinMode(OLED_CS, OUTPUT);
+  pinMode(OLED_RESET, OUTPUT);
+  pinMode(OLED_CD, OUTPUT);
+
+  pinMode(BUTTON, INPUT);
+
+  pinMode(BUZZER, OUTPUT)
 }
 
 void loop(){
+  //when the button is clicked call receive
 
+  //on long press, call clear
+
+  //at basically any random-ish time deliver a prescript, but deliver already accounts for that
 }
 
 
@@ -36,18 +51,50 @@ void deliver(){
   }
 
   isExecuting = true;
+  isReceived = false;
 
   int randNum = random (0, prescriptsLen);
 
   currentPrescript = prescripts[randNum];
 
-  
+  //display(currentPrescript);
+  beep();
 }
 
+//on button click stuff happens
+void onClick(){
+  if (!isExecuting){
+    return;
+  }
+
+  if (!isReceived){
+    isReceived = true;
+  }
+
+  display(currentPrescript)
+}
+
+
 //function for action on clear
+//happens on long click
 void clear(){
+  if (!isExecuting){
+    return;
+  }
+
   isExecuting = false;
 
-  currentPrescipt = 
+  currentPrescipt = "";
+
+  display("_CLEAR._");
+}
+
+//display prescript
+void display(String message){
+
+}
+
+void beep(){
+
 }
 
